0b3470bf18bd90124bfb1f4382e909094b43f3d3
inyoeng <79848622+inyoeng@users.noreply.github.com>
2021-07-28 17:35:09 +0900

    7월 28일

--- Lms/src/main/java/co/micol/lms/command/Command.java
+++
@@ -1,8 +0,0 @@
-package co.micol.lms.command;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Command {
-	public String execute(HttpServletRequest request, HttpServletResponse response);
-}
