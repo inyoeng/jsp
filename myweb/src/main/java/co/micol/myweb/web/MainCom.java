0a2d4e7aa3794777d14ef2ea650ddffc015b15f7
inyoeng <79848622+inyoeng@users.noreply.github.com>
2021-07-29 17:52:01 +0900

    7월 29일

---
+++ myweb/src/main/java/co/micol/myweb/web/MainCom.java
@@ -0,0 +1,37 @@
+package co.micol.myweb.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/main2.do")
+public class MainCom extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+    public MainCom() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String page = "WEB-INF/views/member/memberList.jsp";
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(page);
+		dispatcher.forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ myweb/src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>
+		타일즈를 보여주세요!
+		<p>
+			<a href="main.do">tiles call</a>
+		</p>
+		<a href="main2.do">JSP call</a>
+	</h1>
+</body>
+</html>
